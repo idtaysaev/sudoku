764e613008f62a1f2fbb4f74d433861e625c0c84
idtaysaev <taysaevid@gmail.com>
2019-03-09 22:41:20 +0300

    Create separate components for both rules and controls

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -28,44 +28,8 @@
           Запустить
         </button>
       </div>
-      <div class="sudoku-config-rules">
-        <h2>
-          Правила:
-        </h2>
-        <p>
-          Игровое поле представляет собой квадрат размером 9×9, разделённый на меньшие квадраты со стороной в 3 клетки. Таким образом, всё игровое поле состоит из 81 клетки. В них уже в начале игры стоят некоторые числа (от 1 до 9), называемые подсказками. От игрока требуется заполнить свободные клетки цифрами от 1 до 9 так, чтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3×3 каждая цифра встречалась бы только один раз.
-        </p>
-      </div>
-      <div class="sudoku-config-controls">
-        <h2>
-          Управление:
-        </h2>
-        <p>
-          Ячейки способны окрашиваться в 3 цвета: красный, желтый и зеленый:
-        </p>
-        <ul>
-          <li>
-            Красный указывает на наличие ошибки в ряде / колонке / квадрате 3х3
-          </li>
-          <li>
-            Зеленый указывает на правильную полную заполненность в ряде / колонке / квадрате 3х3
-          </li>
-          <li>
-            Желтый является комбинацией предыдущих 2 случаев
-          </li>
-        </ul>
-        <p>
-          Можно играть как с помощью мышки, так и с помощью клавиатуры:
-        </p>
-        <ul>
-          <li>
-            tab - переключение к следующей ячейке
-          </li>
-          <li>
-            клавиша вверх / клавиша вниз позволяют менять значение текущей ячейки
-          </li>
-        </ul>
-      </div>
+      <sudoku-rules />
+      <sudoku-controls />
     </div>
     <div
       v-if="started"
@@ -119,9 +83,13 @@
 
 <script>
 import Cell from './Cell'
+import SudokuRules from './Rules'
+import SudokuControls from './Controls'
 export default {
   components: {
-    Cell
+    Cell,
+    SudokuRules,
+    SudokuControls
   },
   data () {
     const step = 9
@@ -335,13 +303,6 @@ export default {
 }
 .sudoku-config-initial-value {
   margin-left: 0.4em;
-}
-.sudoku-config-rules,
-.sudoku-config-controls {
-  width: 600px;
-  max-width: 100%;
-  margin: 2em auto 0;
-  text-align: left;
 }
 .sudoku-container {
   // using grid only in demo
